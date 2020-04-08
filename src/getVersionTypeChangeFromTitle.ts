8fe3267a85f0cb20328330906cfa1f5ae5a574c2
Lukas Pastuszek <lukas.pastuszek@pipedrive.com>
2020-04-08 21:30:12 +0200

    Dummy2

--- src/getVersionTypeChangeFromTitle.ts
+++ src/getVersionTypeChangeFromTitle.ts
@@ -19,19 +19,19 @@ export const getVersionTypeChangeFromTitle = (title: string): VersionType => {
       throw new Error(`Expected next version to be in format X.X.X. Found "${nextVersion}"`);
     }
 
-    if (!(previousVersion[0] >= nextVersion[0] && previousVersion[1] >= nextVersion[1] && previousVersion[2] >= nextVersion[2])) {
+    if (previousVersion[0] >= nextVersion[0] && previousVersion[1] >= nextVersion[1] && previousVersion[2] >= nextVersion[2]) {
       throw new Error(`Expected previous version to be smaller in PR title "${title}"`);
     }
 
-    if (parsedPrevious[0] > parsedNext[0]) {
+    if (parsedNext[0] > parsedPrevious[0]) {
       return 'MAJOR';
     }
 
-    if (parsedPrevious[1] > parsedNext[1]) {
+    if (parsedNext[1] > parsedPrevious[1]) {
       return 'MINOR'
     }
 
-    if (parsedPrevious[2] > parsedNext[2]) {
+    if (parsedNext[2] > parsedPrevious[2]) {
       return 'PATCH'
     }
 
